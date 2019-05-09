426645a33500074f37750982279009d0f2df655e
liu.chengbiao <liu.chengbiao@ustcinfo.com>
2019-05-09 19:37:56 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <flow></flow>
+  </div>
+</template>
+
+<script>
+  import Flow from '@/index'
+
+  export default {
+    name: 'App',
+    components: {
+      Flow
+    }
+  }
+</script>
